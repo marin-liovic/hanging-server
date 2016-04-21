aa76d28ed8b0108fc74219a5b47cd3958d10c86e
Aaron Pollack <pollackaaron@gmail.com>
2016-04-21 10:16:49 -0700

    Added support for all routes

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ var app = express();
 var PORT = 8081;
 
 app.get('/*', function(req, res, next) {
-  console.log('request received');
+  console.log('GET request received');
+});
+
+app.post('/*', function(req, res, next) {
+  console.log('POST request received');
+});
+
+app.put('/*', function(req, res, next) {
+  console.log('PUT request received');
+});
+
+app.delete('/*', function(req, res, next) {
+  console.log('DELETE request received');
 });
 
 app.listen(PORT);
