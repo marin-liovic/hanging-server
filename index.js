b35374631480f647a9948333c3e67396b9f50346
Aaron Pollack <pollackaaron@gmail.com>
2016-04-21 10:15:01 -0700

    Make get route greedy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var PORT = 8081;
 
-app.get('/', function(req, res, next) {
+app.get('/*', function(req, res, next) {
   console.log('request received');
 });
 
